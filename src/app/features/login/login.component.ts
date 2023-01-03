41dbe8121d670d94f61d03084eb59b9dfd7ea676
leandrobr95 <leandrobsb55@gmail.com>
2023-01-02 22:21:59 -0300

    feat: criando layout para tela de login

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: []
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+  passwordVisible!: boolean;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = this.buildLoginForm();
   }
+
+  buildLoginForm(): FormGroup {
+    return this.formBuilder.group(
+      {
+        emailUsuario: [null, [Validators.required]],
+        senhaUsuario: [null, [Validators.required]]
+      }
+    );
+  }
+
+  login(): void { }
 
 }
--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzCardModule } from 'ng-zorro-antd/card';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
@@ -10,6 +18,14 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+
+    NzButtonModule,
+    NzIconModule,
+    NzTypographyModule,
+    NzFormModule,
+    NzInputModule,
+    NzCardModule,
 
     LoginRoutingModule
   ]
