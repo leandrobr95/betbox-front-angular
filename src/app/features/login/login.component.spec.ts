d2468d52d1a21b6af8c84af9f65f3bbf8f7236f4
leandrobr95 <leandrobsb55@gmail.com>
2023-01-03 09:08:51 -0300

    test: atualizando módulos e criando testes unitários

--- src/app/features/login/login.component.spec.ts
+++ src/app/features/login/login.component.spec.ts
@@ -1,4 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzCardModule } from 'ng-zorro-antd/card';
 
 import { LoginComponent } from './login.component';
 
@@ -8,9 +17,20 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [LoginComponent],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+
+        NzButtonModule,
+        NzIconModule,
+        NzTypographyModule,
+        NzFormModule,
+        NzInputModule,
+        NzCardModule,
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +41,18 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create the login form', () => {
+    const loginForm = component.buildLoginForm();
+
+    expect(loginForm.controls.userMail.value).toBeNull();
+    expect(loginForm.controls.userPassword.value).toBeNull();
+  });
+
+  it('should start invalid login form', () => {
+    const loginForm = component.buildLoginForm();
+
+    expect(loginForm.valid).toBeFalse();
   });
 });
